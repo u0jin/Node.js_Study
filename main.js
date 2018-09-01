0b5fa415aec04bc8a2068e6a48a9f484966cb481
ujin <yujin5836@gmail.com>
2018-09-01 20:44:34 +0900

    1st Node.js : Start

---
+++ main.js
@@ -0,0 +1,28 @@
+
+/**
+ * main.js
+ * @returns
+ */
+
+var second=require("./second.js");
+
+function f1(){
+	
+	console.log("a1 함수호출");
+	
+}
+
+
+
+
+
+console.log("HelloWorld");
+
+var a1= 100;
+var a2=" 문자열1";
+
+console.log("a1: %d, a2: %s",a1,a2);
+
+console.log("a1:",a1);
+console.log("a1:", a1,"a2:",a2);
+f1(); second.f2();
---
+++ second.js
@@ -0,0 +1,8 @@
+/**
+ * second.js
+ * 모듈만들꺼얌
+ */
+
+exports.f2=function(){
+	console.log("f2 함수 호출");
+};
